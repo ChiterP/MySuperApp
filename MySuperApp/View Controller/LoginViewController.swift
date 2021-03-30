addf477442f3abf85bcd3743bc6a458226e7589d
Sergeytsev A <Sergeytsev A>
2021-03-30 16:00:57 +0300

    осталось подсушить код и сделать красоту.

--- MySuperApp/View Controller/LoginViewController.swift
+++ MySuperApp/View Controller/LoginViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
@@ -18,25 +16,37 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var forgotUserNameButton: UIButton!
     @IBOutlet weak var forgotPasswordButton: UIButton!
     
-    
     private let userName = "User"
     private let userPassword = "Passw"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     //MARK: - Override methods
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard  let loginVC = segue.destination as? WelcomeViewController else { return }
         loginVC.userName = userNameTF.text
     }
     
-    
     //MARK: - IB Actions
     @IBAction func logInBTAction() {
-        if userNameTF.text == userName {
-            
+        if userNameTF.text != "" {
+            if userNameTF.text == userName {
+            } else {
+                showAlert(with: "Неверное имя!", and:  "")
+            }
+        } else {
+            showAlert(with: "Введите имя!", and:  "")
+        }
+        
+        if passwordTF.text != "" {
+            if passwordTF.text == userPassword {
+            } else {
+                showAlert(with: "Неверный пароль!", and:  "")
+            }
+        } else {
+            showAlert(with: "Введите пароль!", and:  "")
         }
     }
     
@@ -47,7 +57,15 @@ class LoginViewController: UIViewController {
     @IBAction func forGotUserNameBTAction() {
         showAlert(with: "Введите имя:", and:  "\(userName)")
     }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard segue.source is WelcomeViewController else { return }
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
 }
+
 
 extension LoginViewController {
     private func showAlert(with title: String, and message: String) {
@@ -57,3 +75,5 @@ extension LoginViewController {
         present(alert, animated: true)
     }
 }
+
+
